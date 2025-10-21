9d21427c477d84848be0431b220548eee7d5298e
Dylan Phuong <phuongbd40812ts@gmail.com>
2025-10-21 21:51:00 +0700

    video 53

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -3,13 +3,21 @@ const HomePage = () => {
     return (
         <div className="homepage-container">
 
-            <video w autoPlay muted loop>
+            <video autoPlay muted loop>
                 <source
                     src={videoHomepage}
                     type="video/mp4"
                 />
             </video>
+            <div className='homepage-content'>
+                <div className='title-1'>Truong dai hoc Su Pham Ky Thuat</div>
+                <div className='title-2'> so 1 vo van ngan</div>
+                <div className='title-3'>
+                    <button>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
+
     );
 }
 
